c4cb960ff51e3494d1013f9a39af0a237361b502
Caio De Albuquerque Lucena Medeiros <camedeiros1@stefanini.com>
2017-04-12 11:15:10 -0300

    - Implementação de conexão com o websocket

--- app.js
+++ app.js
@@ -2,6 +2,21 @@
 var app = require('./config/server');
 
 /* Porta de acesso ao server */
-app.listen(3000, function() {
+var server = app.listen(3000, function() {
     console.log("Servidor online");
+});
+
+/* Servidor Socket*/
+var io = require('socket.io').listen(server);
+
+app.set('io', io);
+
+/* Criar conexão por websocket */
+io.on('connection', function(socket) {
+    console.log('Usuário Conectou');
+
+    socket.on('disconnect', function() {
+        console.log("O usuário desconectou");
+    });
+
 });
